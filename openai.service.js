b5335fef4c1ddc1b240279258df859eb66752371
rmonterr12 <rodolfo.monterroso@editorialsl.com>
2024-08-15 09:38:59 -0600

    Add files via upload

---
+++ openai.service.js
@@ -0,0 +1,34 @@
+const fs = require("fs");
+const createAssistant = async (openai) => {
+  // Assistant file path
+  const assistantFilePath = "assistant.json";
+  // check if file exists
+  if (!fs.existsSync(assistantFilePath)) {
+    // Create a file
+    const file = await openai.files.create({
+      file: fs.createReadStream("knowledge.docx"),
+      purpose: "assistants",
+    });
+    // Create a vector store including our file
+    let vectorStore = await openai.beta.vectorStores.create({
+      name: "Chat Demo",
+      file_ids: [file.id],
+    });
+    // Create assistant
+    const assistant = await openai.beta.assistants.create({
+      name: "Asistente de Mate 5o diver",
+      instructions: `Eres el asistente tipo tutor virtual de un libro de matemáticas de 5o. bachillerato, ayudarás a los profesores a crear preguntas, explicaciones de temas, planificaciones educativas, y darás información general del libro que se encuentra en el documento del mismo. `,
+      tools: [{ type: "file_search" }],
+      tool_resources: { file_search: { vector_store_ids: [vectorStore.id] } },
+      model: "gpt-4o",
+    });
+    // Write assistant to file
+    fs.writeFileSync(assistantFilePath, JSON.stringify(assistant));
+    return assistant;
+  } else {
+    // Read assistant from file
+    const assistant = JSON.parse(fs.readFileSync(assistantFilePath));
+    return assistant;
+  }
+};
+module.exports = { createAssistant };
